90432b2aeb17fcd60906f745a3a4dc7691a79deb
Dhruv <dhruvanandsingh2004@gmail.com>
2024-07-24 23:52:53 +0530

    changed styling

--- client/src/pages/user/UserProfile.jsx
+++ client/src/pages/user/UserProfile.jsx
@@ -8,7 +8,7 @@ const UserProfile = () => {
       <div className='h-[55vh] rounded-lg shadow-lg relative w-[40vw] px-6'>
         <h1 className='relative text-6xl top-[-20px]'>Profile</h1>
         <div>
-            <div className='flex-col justify-between w-2/3'>
+            <div className='flex-col justify-between h-full w-2/3'>
                 <input type='text' placeholder='Name' id='name' className='border-b-2 w-[15vw] outline-none' />
                 <input type='text' placeholder='Name' id='name' className='border-b-2 w-[15vw] outline-none' />
                 <input type='text' placeholder='Name' id='name' className='border-b-2 w-[15vw] outline-none' />
